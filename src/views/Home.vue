64f665d8853a56133b3483cb5372ebd5d6f5637d
josh-web <josh@jseidel.dev>
2021-07-25 12:05:00 +0200

    added styling to home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,131 @@
 <template>
-  <div class="home"></div>
+  <div class="home container">
+    <!-- Header -->
+    <div class="header flex">
+      <div class="left flex flex-column">
+        <h1>Rechnungen</h1>
+        <span>Es sind 4 Rechnungen vorhanden</span>
+      </div>
+      <div class="right flex">
+        <div @click="toggleFilterMenu" class="filter flex">
+          <span>Nach Status filtern</span>
+          <img src="@/assets/icon-arrow-down.svg" alt="" />
+          <ul v-show="filterMenu" class="filter-menu">
+            <li>Vorlage</li>
+            <li>Offen</li>
+            <li>Bezahlt</li>
+            <li>Filter zurücksetzen</li>
+          </ul>
+        </div>
+        <div @click="newInvoices" class="button flex">
+          <div class="inner-button flex">
+            <img src="@/assets/icon-plus.svg" alt="" />
+          </div>
+          <span>Neue Rechnung</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Home",
+  data() {
+    return {
+      filterMenu: null,
+    };
+  },
   components: {},
+  methods: {
+    newInvoice() {},
+    toggleFilterMenu() {
+      this.filterMenu = !this.filterMenu;
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  color: #fff;
+
+  .header {
+    margin-bottom: 65px;
+
+    .left,
+    .right {
+      flex: 1;
+    }
+
+    .right {
+      justify-content: flex-end;
+      align-items: center;
+
+      .button,
+      .filter {
+        align-items: center;
+
+        span {
+          font-size: 12px;
+        }
+      }
+
+      .filter {
+        position: relative;
+        margin-right: 40px;
+        cursor: pointer;
+
+        img {
+          margin-left: 12px;
+          width: 9px;
+          height: 5px;
+        }
+
+        .filter-menu {
+          width: 120px;
+          position: absolute;
+          top: 25px;
+          list-style: none;
+          background-color: #1e2139;
+          box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+            0 2px 4px -1px rgba(0, 0, 0, 0.06);
+
+          li {
+            cursor: pointer;
+            font-size: 12px;
+            padding: 10px 20px;
+
+            &:hover {
+              color: #1e2139;
+              background-color: #fff;
+            }
+          }
+        }
+      }
+      .filter {
+      }
+
+      .button {
+        padding: 8px 10px;
+        background-color: #7c5dfa;
+        border-radius: 40px;
+
+        .inner-button {
+          margin-right: 8px;
+          border-radius: 50%;
+          padding: 8px;
+          align-items: center;
+          justify-content: center;
+          background-color: #fff;
+
+          img {
+            width: 10px;
+            height: 10px;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
