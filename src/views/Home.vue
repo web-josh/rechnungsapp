8ffc36b76f28bfc29f52f8cd2748d2bf54715213
josh-web <josh@jseidel.dev>
2021-07-30 11:31:29 +0200

    added invoice component and no invoice fallback

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,11 +25,25 @@
         </div>
       </div>
     </div>
+    <!-- Invoices -->
+    <div v-if="invoiceData.length > 0">
+      <Invoice
+        v-for="(invoice, index) in invoiceData"
+        :invoice="invoice"
+        :key="index"
+      />
+    </div>
+    <div v-else class="empty flex flex-column">
+      <img src="@/assets/illustration-empty.svg" alt="" />
+      <h3>Keine Einträge vorhanden.</h3>
+      <p>Erstelle Einträge, indem du auf neue Einträge klickst.</p>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapMutations } from "vuex";
+import Invoice from "../components/Invoice";
+import { mapMutations, mapState } from "vuex";
 export default {
   name: "Home",
   data() {
@@ -37,7 +51,9 @@ export default {
       filterMenu: null,
     };
   },
-  components: {},
+  components: {
+    Invoice,
+  },
   methods: {
     ...mapMutations(["TOGGLE_INVOICE"]),
     newInvoice() {
@@ -46,6 +62,9 @@ export default {
     toggleFilterMenu() {
       this.filterMenu = !this.filterMenu;
     },
+  },
+  computed: {
+    ...mapState(["invoiceData"]),
   },
 };
 </script>
@@ -129,6 +148,29 @@ export default {
           }
         }
       }
+    }
+  }
+
+  .empty {
+    margin-top: 160px;
+    align-items: center;
+
+    img {
+      width: 214px;
+      height: 200px;
+    }
+
+    h3 {
+      font-size: 20px;
+      margin-top: 40px;
+    }
+
+    p {
+      text-align: center;
+      max-width: 224px;
+      font-size: 12px;
+      font-weight: 300;
+      margin-top: 16px;
     }
   }
 }
